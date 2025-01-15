b8ff95497beceb165c58ea11516f31f13aede3bc
Rinkit Adhana <therinkit@gmail.com>
2025-01-16 02:37:52 +0530

    comp added

---
+++ client/src/Pages/Category.tsx
@@ -0,0 +1,9 @@
+const Category = () => {
+  return (
+    <section className=" flex justify-end">
+      <div className=" border rounded-md px-3 py-0.5">option</div>
+    </section>
+  )
+}
+
+export default Category
