87295f3b88cd9c8673c1137ab88a450f1d35cd67
Rinkit Adhana <therinkit@gmail.com>
2025-01-16 01:36:11 +0530

    post button completed

--- client/src/Components/PostButtom.tsx
+++ client/src/Components/PostButtom.tsx
@@ -1,5 +1,14 @@
+import { LuSquarePen } from "react-icons/lu"
+
 const PostButtom = () => {
-  return <button className=" fixed right-2 bottom-2">post</button>
+  return (
+    <button
+      title="post"
+      className=" fixed right-5 bottom-5 text-xl border border-zinc-600 rounded-md p-2 bg-red-500 hover:bg-transparent transition-colors duration-200 select-none"
+    >
+      <LuSquarePen />
+    </button>
+  )
 }
 
 export default PostButtom
