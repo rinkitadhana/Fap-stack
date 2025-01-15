c5c9146b34162f3b1fd7c375d9e0e45121cf7cd8
Rinkit Adhana <therinkit@gmail.com>
2025-01-16 02:37:59 +0530

    comp added

--- client/src/Pages/Post.tsx
+++ client/src/Pages/Post.tsx
@@ -1,8 +1,25 @@
 const Post = () => {
   return (
-    <section className=" border border-zinc-600 rounded h-[150px]">
-      Post
-      <p></p>
+    <section className=" flex flex-col border border-zinc-600 rounded p-3">
+      <div className=" flex  ">
+        <div className=" ">
+          <img
+            className=" size-9 rounded-full"
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
+          />
+        </div>
+        <div className="w-[90%] px-3">
+          <input
+            placeholder="Your homies are waiting for your link!"
+            className="post-input"
+          />
+        </div>
+      </div>
+      <div className=" flex justify-end">
+        <button className=" select-none border border-zinc-600 bg-red-500 hover:bg-transparent transition duration-200 font-medium text-white rounded-md px-3 py-1">
+          Post
+        </button>
+      </div>
     </section>
   )
 }
