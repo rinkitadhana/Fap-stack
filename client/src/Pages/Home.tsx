26f16da1524179c4d16cfb9fb18af8b31aea63b9
Rinkit Adhana <therinkit@gmail.com>
2025-01-17 06:18:40 +0530

    home page edit

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,13 +1,15 @@
-import FapPost from "../Components/FapPost";
+import FapPost from "../Components/FapPost"
+import PostBox from "../Components/PostBox"
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-2">
-      <FapPost />
-      <FapPost />
+      <PostBox />
+      <PostBox />
+      <PostBox />
       <FapPost />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
