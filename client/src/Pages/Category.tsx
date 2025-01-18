7adf6ea8b9e5f2cefe658c00645a24f4a27d0d5f
Rinkit Adhana <therinkit@gmail.com>
2025-01-18 22:28:10 +0530

    section completed

--- client/src/Pages/Category.tsx
+++ client/src/Pages/Category.tsx
@@ -1,7 +1,12 @@
 const Category = () => {
   return (
-    <section className=" flex justify-end">
-      <div className=" border rounded-md px-3 py-0.5">option</div>
+    <section className="border border-zinc-700 rounded-md p-3 flex items-center gap-3">
+      <span>Filter :</span>
+      <div className="flex flex-wrap gap-2">
+        <button className=" cat-btn">Latest</button>
+        <button className=" cat-btn">Popular</button>
+        <button className=" cat-btn">Oldest</button>
+      </div>
     </section>
   )
 }
