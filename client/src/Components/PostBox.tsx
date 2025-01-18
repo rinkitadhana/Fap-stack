b37502bfe4a0538c8e28ea78cef154a16cecd374
Rinkit Adhana <therinkit@gmail.com>
2025-01-18 21:06:35 +0530

    Enhance PostBox component with VPN status indicator and button styling
    - Added VPN status indicator with dynamic styling based on state.
    - Updated button styles for like and dislike actions.
    - Improved layout and hover effects for image and links.

--- client/src/Components/PostBox.tsx
+++ client/src/Components/PostBox.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react"
+import { BiDislike } from "react-icons/bi"
+import { BiLike } from "react-icons/bi"
+import { FiShare } from "react-icons/fi"
+import { MdOutlineVpnLock } from "react-icons/md"
+
 const PostBox = () => {
+  const [vpn, setVpn] = useState(true)
+
   const link =
     "https://github.com/rinkitadhana/Fap-stack//github.com/rinkitadhana/Fap-stack"
   return (
@@ -15,27 +23,45 @@ const PostBox = () => {
           </div>
           <div className=" text-sm opacity-75">• 15 hrs ago</div>
         </div>
-        <div>Goto</div>
+        <div
+          className={`select-none flex gap-1 items-center  rounded-md px-2.5 py-1 ${
+            vpn
+              ? "text-green-400  bg-green-400/10"
+              : "text-red-400  bg-red-400/10"
+          } `}
+        >
+          <span className=" animate-pulse">
+            <MdOutlineVpnLock />
+          </span>
+          <span>VPN</span>
+        </div>
       </div>
       <div className=" select-none ">
-        <div className="hover:brightness-75 transition-all duration-300 cursor-pointer relative">
+        <div className="hover:brightness-75 transition-all duration-200 cursor-pointer relative">
           <img
-            className="  rounded-md w-full h-[270px] object-cover"
+            className="  rounded-md w-full h-[280px] object-cover"
             src="https://i.pinimg.com/736x/18/97/29/189729516ae44062a9d98e74b7394dcf.jpg"
           />
           <a className=" absolute bottom-3 mx-3 bg-black/50 backdrop-blur-[2px] px-2 py-1 rounded-md">
             {link}
           </a>
         </div>
-        <span className=" text-sm opacity-75">From loda.io</span>
+        <a href="" className=" text-sm opacity-75">
+          From loda.io
+        </a>
       </div>
-      <div className=" flex gap-2">
-        <div className=" border border-zinc-600 rounded-md py-1 px-3">
-          Indian
-        </div>
-        <div className=" border border-zinc-600 rounded-md py-1 px-3">vpn</div>
+      <div className=" flex gap-2"></div>
+      <div className=" flex justify-end gap-4  px-4 items-center">
+        <button className=" btn">
+          <BiLike />
+        </button>
+        <button className=" btn">
+          <BiDislike />
+        </button>
+        <button className=" text-xl">
+          <FiShare />
+        </button>
       </div>
-      <div></div>
     </div>
   )
 }
