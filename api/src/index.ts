5e92c4d250621fc832389c3a2a5cb4aa18d3c564
Rinkit Adhana <therinkit@gmail.com>
2025-01-21 03:07:17 +0530

    backend setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
+
 app.get("/", (req, res) => {
   res.send("fap-stack")
 })
