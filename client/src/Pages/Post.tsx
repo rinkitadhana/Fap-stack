ea090b33020f2546be35680271c35f10b1cba576
Rinkit Adhana <therinkit@gmail.com>
2025-01-25 03:13:19 +0530

    error solved

--- client/src/Pages/Post.tsx
+++ client/src/Pages/Post.tsx
@@ -6,7 +6,7 @@ const Post = () => {
   const [hasValue, setHasValue] = useState(false)
   const [hasHttps, setHasHttps] = useState(false)
   const [hasDomain, setHasDomain] = useState(false)
-  const [preview, setPreview] = useState(null)
+  const [preview, setPreview] = useState<string | null>(null)
   const defaultImage =
     "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
 
