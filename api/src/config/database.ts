fba3aa25119398232d1fe402e3c1f7d8f614d386
Rinkit Adhana <therinkit@gmail.com>
2025-01-25 05:28:57 +0530

    ocd

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose"
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI)
-    console.log("MongoDB Connected!")
+    console.log("MongoDB connected!")
   } catch (error) {
-    console.log("MongoDB Error: ", error)
+    console.log("MongoDB error: ", error)
     process.exit(1)
   }
 }
