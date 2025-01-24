f88b4d942e1a1f59d35f93aa757b65ca9c966847
Rinkit Adhana <therinkit@gmail.com>
2025-01-25 03:12:53 +0530

    category changed and sync with post

--- client/src/Pages/Category.tsx
+++ client/src/Pages/Category.tsx
@@ -1,7 +1,9 @@
 const Category = () => {
   return (
-    <section className="border border-zinc-700 rounded-md p-3 flex items-center gap-3">
-      <span>Filter :</span>
+    <section className="border h-full border-zinc-700 rounded-md p-3 flex flex-col gap-2">
+      <h1 className=" text-lg opacity-60 font-semibold">
+        Filter & Categories.
+      </h1>
       <div className="flex flex-wrap gap-2">
         <button className=" cat-btn">Latest</button>
         <button className=" cat-btn">Popular</button>
@@ -12,3 +14,5 @@ const Category = () => {
 }
 
 export default Category
+{
+}
