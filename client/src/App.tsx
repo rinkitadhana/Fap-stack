8da0ced1f79b3b68d228c5512dbfa1cc47a7f58f
Rinkit Adhana <therinkit@gmail.com>
2025-01-25 03:10:39 +0530

    structure improved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,26 @@
-import Footer from "./Components/Footer"
-import Navbar from "./Components/Navbar"
-import PostButtom from "./Components/PostButtom"
+import Footer from "./Components/PageComponents/Footer"
+import Navbar from "./Components/PageComponents/Navbar"
+import PostButtom from "./Components/PageComponents/PostButtom"
 import Screen from "./Layout/Screen"
 import Category from "./Pages/Category"
-import Home from "./Pages/Home"
 import Post from "./Pages/Post"
+import IndexPage from "./Pages/IndexPage"
 
 const App = () => {
   return (
     <Screen>
       <div className=" flex flex-col gap-2">
         <Navbar />
-        <Post />
-        <Category />
-        <Home />
+        <div className=" flex md:flex-row flex-col gap-2">
+          <div className=" md:basis-1/2">
+            <Post />
+          </div>
+          <div className=" md:basis-1/2">
+            <Category />
+          </div>
+        </div>
+
+        <IndexPage />
         <Footer />
       </div>
       <PostButtom />
