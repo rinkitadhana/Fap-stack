bfcc645878b808d2563f975b51d4664bb0bb2974
itSarthak <sarthakt3006@gmail.com>
2025-01-26 19:05:56 +0530

    Finalized sorting based on given parameters on frontend

--- client/src/Pages/IndexPage.tsx
+++ client/src/Pages/IndexPage.tsx
@@ -5,13 +5,35 @@ interface HomeProps {
 }
 
 const Home = ({ categories }: HomeProps) => {
+  let totalPosts = posts;
+  switch (categories) {
+    case "Latest": {
+      totalPosts.sort((a, b) => a.timePosted - b.timePosted);
+      break;
+    }
+    case "Popular": {
+      totalPosts.sort(
+        (a, b) => b.numLikes + b.numDislikes - (a.numLikes + a.numDislikes)
+      );
+      break;
+    }
+    case "Oldest": {
+      totalPosts.sort((a, b) => b.timePosted - a.timePosted);
+      break;
+    }
+    case "": {
+      totalPosts = posts;
+      break;
+    }
+  }
   return (
     <div className="grid md:grid-cols-3 grid-cols-1 md:place-items-center gap-2">
       {/* {Array.from({ length: 6 }, (_, i) => (
         <PostBox key={i} />
       ))} */}
-      {!categories &&
-        posts.map((post, index) => <PostBox key={index} {...post} />)}
+      {posts.map((post, index) => (
+        <PostBox key={index} {...post} />
+      ))}
     </div>
   );
 };
