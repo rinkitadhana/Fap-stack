2a04a6fa609eb6c0ecf2444ae5286f3bc995e7d0
Rinkit Adhana <therinkit@gmail.com>
2025-01-27 18:00:07 +0530

    deploy error

--- client/src/components/ui/border-beam.tsx
+++ client/src/components/ui/border-beam.tsx
@@ -1,14 +1,14 @@
-import { cn } from "@/lib/utils";
-import { ComponentPropsWithoutRef } from "react";
+import { cn } from "@/lib/utils"
+import { ComponentPropsWithoutRef } from "react"
 
 interface BorderBeamProps extends ComponentPropsWithoutRef<"div"> {
-  size?: number;
-  duration?: number;
-  borderWidth?: number;
-  anchor?: number;
-  colorFrom?: string;
-  colorTo?: string;
-  delay?: number;
+  size?: number
+  duration?: number
+  borderWidth?: number
+  anchor?: number
+  colorFrom?: string
+  colorTo?: string
+  delay?: number
 }
 
 export const BorderBeam = ({
@@ -43,9 +43,9 @@ export const BorderBeam = ({
 
         // pseudo styles
         "after:absolute after:aspect-square after:w-[calc(var(--size)*1px)] after:animate-border-beam after:[animation-delay:var(--delay)] after:[background:linear-gradient(to_left,var(--color-from),var(--color-to),transparent)] after:[offset-anchor:calc(var(--anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(var(--size)*1px))]",
-        className,
+        className
       )}
       {...props}
     />
-  );
-};
+  )
+}
