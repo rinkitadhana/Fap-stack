f416c2b704d5f8ec82eea6dabe8d5bf7fb9cd141
itSarthak <sarthakt3006@gmail.com>
2025-01-28 22:02:30 +0530

    Added Border to the avatar modal

--- client/src/Pages/ImagesModal.tsx
+++ client/src/Pages/ImagesModal.tsx
@@ -13,13 +13,13 @@ function Modal({ isOpen, onClose, title, setPreview }: ModalProps) {
   return (
     <div
       onClick={onClose}
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+      className="fixed inset-0 z-50 flex  items-center justify-center bg-black/50"
     >
       <motion.div
         initial={{ opacity: 0, scale: 0 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0 }}
-        className="w-full max-w-md rounded-2xl shadow-lg p-6 relative bg-mainBlack/80 backdrop-blur-lg flex flex-col gap-5"
+        className=" w-full max-w-md rounded-2xl shadow-lg p-6 relative border-borderColor border-2 bg-mainBlack/80 backdrop-blur-lg flex flex-col gap-5"
       >
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-semibold text-white select-none">
