11578f8693145b147a6f7c51b21aafaad4353f55
Rinkit Adhana <therinkit@gmail.com>
2025-01-30 16:30:21 +0530

    post controller updated

--- api/src/controllers/post-controller.ts
+++ api/src/controllers/post-controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express"
-import Post from "models/post-model"
-import PostStats from "models/postStats-model"
-import errorHandler from "utils/errorHandler"
+import Post from "../models/post-model"
+import PostStats from "../models/postStats-model"
+import errorHandler from "../utils/errorHandler"
 
-const PostHandler = async (req: Request, res: Response) => {
+const postHandler = async (req: Request, res: Response) => {
   try {
     const { username, link, avatar, thumbnail, banner } = req.body
     if (!link) {
       res.status(400).json({ message: "Link isn't provided!" })
+      return
+    }
+    const linkCheck = await Post.findOne({ link })
+    if (linkCheck) {
+      res.status(400).json({ message: "Link already exists" })
       return
     }
     const newPost = new Post({
@@ -32,4 +37,4 @@ const PostHandler = async (req: Request, res: Response) => {
   }
 }
 
-export { PostHandler }
+export { postHandler }
